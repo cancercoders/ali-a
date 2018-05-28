612ebfd4a715ad6eb302d527797348491a12847f
RealVaranox <30459307+RealVaranox@users.noreply.github.com>
2018-05-28 16:24:34 +1200

    Update w o a h.bat

--- w o a h.bat
+++ w o a h.bat
@@ -12,3 +12,47 @@ timeout 6 /nobreak
 start intro.mp4
 timeout 6 /nobreak
 start intro.mp4
+echo *NEW* SHOTGUNS ONLY MODE in Fortnite: Battle Royale!
+timeout 0.5 /nobreak
+echo Ali-A's MOST KILLS on Fortnite: Battle Royale!
+timeout 0.5 /nobreak
+echo The SNIPER SCAR in Fortnite: Battle Royale!
+timeout 0.5 /nobreak
+echo What's INSIDE the *METEOR* in Fortnite: Battle Royale...?
+timeout 0.5 /nobreak
+echo Ali-A REACTS to his FIRST EVER Fortnite game! (FUNNY)
+timeout 0.5 /nobreak
+echo WINNING Fortnite: Battle Royale IN THE STORM...! (CRAZY)
+echo *NEW* SHOTGUNS ONLY MODE in Fortnite: Battle Royale!
+timeout 0.5 /nobreak
+echo Ali-A's MOST KILLS on Fortnite: Battle Royale!
+timeout 0.5 /nobreak
+echo The SNIPER SCAR in Fortnite: Battle Royale!
+timeout 0.5 /nobreak
+echo What's INSIDE the *METEOR* in Fortnite: Battle Royale...?
+timeout 0.5 /nobreak
+echo Ali-A REACTS to his FIRST EVER Fortnite game! (FUNNY)
+timeout 0.5 /nobreak
+echo WINNING Fortnite: Battle Royale IN THE STORM...! (CRAZY)
+echo *NEW* SHOTGUNS ONLY MODE in Fortnite: Battle Royale!
+timeout 0.5 /nobreak
+echo Ali-A's MOST KILLS on Fortnite: Battle Royale!
+timeout 0.5 /nobreak
+echo The SNIPER SCAR in Fortnite: Battle Royale!
+timeout 0.5 /nobreak
+echo What's INSIDE the *METEOR* in Fortnite: Battle Royale...?
+timeout 0.5 /nobreak
+echo Ali-A REACTS to his FIRST EVER Fortnite game! (FUNNY)
+timeout 0.5 /nobreak
+echo WINNING Fortnite: Battle Royale IN THE STORM...! (CRAZY)
+echo *NEW* SHOTGUNS ONLY MODE in Fortnite: Battle Royale!
+timeout 0.5 /nobreak
+echo Ali-A's MOST KILLS on Fortnite: Battle Royale!
+timeout 0.5 /nobreak
+echo The SNIPER SCAR in Fortnite: Battle Royale!
+timeout 0.5 /nobreak
+echo What's INSIDE the *METEOR* in Fortnite: Battle Royale...?
+timeout 0.5 /nobreak
+echo Ali-A REACTS to his FIRST EVER Fortnite game! (FUNNY)
+timeout 0.5 /nobreak
+echo WINNING Fortnite: Battle Royale IN THE STORM...! (CRAZY)
