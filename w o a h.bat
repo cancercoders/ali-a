54a57b12a18c9f1a607966ae8945d693cbea944e
RealVaranox <30459307+RealVaranox@users.noreply.github.com>
2018-05-28 16:31:15 +1200

    Update w o a h.bat

--- w o a h.bat
+++ w o a h.bat
@@ -13,46 +13,49 @@ start intro.mp4
 timeout 6 /nobreak
 start intro.mp4
 echo *NEW* SHOTGUNS ONLY MODE in Fortnite: Battle Royale!
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo Ali-A's MOST KILLS on Fortnite: Battle Royale!
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo The SNIPER SCAR in Fortnite: Battle Royale!
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo What's INSIDE the *METEOR* in Fortnite: Battle Royale...?
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo Ali-A REACTS to his FIRST EVER Fortnite game! (FUNNY)
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo WINNING Fortnite: Battle Royale IN THE STORM...! (CRAZY)
 echo *NEW* SHOTGUNS ONLY MODE in Fortnite: Battle Royale!
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo Ali-A's MOST KILLS on Fortnite: Battle Royale!
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo The SNIPER SCAR in Fortnite: Battle Royale!
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo What's INSIDE the *METEOR* in Fortnite: Battle Royale...?
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo Ali-A REACTS to his FIRST EVER Fortnite game! (FUNNY)
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo WINNING Fortnite: Battle Royale IN THE STORM...! (CRAZY)
+timeout 1 /nobreak
 echo *NEW* SHOTGUNS ONLY MODE in Fortnite: Battle Royale!
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo Ali-A's MOST KILLS on Fortnite: Battle Royale!
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo The SNIPER SCAR in Fortnite: Battle Royale!
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo What's INSIDE the *METEOR* in Fortnite: Battle Royale...?
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo Ali-A REACTS to his FIRST EVER Fortnite game! (FUNNY)
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo WINNING Fortnite: Battle Royale IN THE STORM...! (CRAZY)
+timeout 1 /nobreak
 echo *NEW* SHOTGUNS ONLY MODE in Fortnite: Battle Royale!
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo Ali-A's MOST KILLS on Fortnite: Battle Royale!
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo The SNIPER SCAR in Fortnite: Battle Royale!
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo What's INSIDE the *METEOR* in Fortnite: Battle Royale...?
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo Ali-A REACTS to his FIRST EVER Fortnite game! (FUNNY)
-timeout 0.5 /nobreak
+timeout 1 /nobreak
 echo WINNING Fortnite: Battle Royale IN THE STORM...! (CRAZY)
+pause
